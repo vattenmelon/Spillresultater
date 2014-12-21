68e464a80d5fde16ca02a4a7f9682ee7453d02b6
Erling Reizer <reizer@gmail.com>
2014-12-21 23:43:57 +0100

    added files i forgot in previous commit

---
+++ Tipperesultater/DataModel/ResultData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipperesultater.Data
+{
+    /// <summary>
+    /// Generic group data model.
+    /// </summary>
+    public class ResultatData
+    {
+
+        public static Dictionary<string, string> spill = new Dictionary<string, string>()
+        {
+                { "lotto", "https://www.norsk-tipping.no/api-lotto/getResultInfo.json"},
+                { "vikinglotto", "https://www.norsk-tipping.no/api-vikinglotto/getResultInfo.json"},
+                { "joker", "https://www.norsk-tipping.no/api-joker/getResultInfo.json"},
+                { "eurojackpot", "https://www.norsk-tipping.no/api-eurojackpot/getResultInfo.json"},
+                { "fotballtipping", "https://www.norsk-tipping.no/api-tipping/getResultInfo.json?gameDay=100"}, //100 = lørdag, 010 = søndag, 001 = onsdag/midtuke
+                { "fotballtippingSon", "https://www.norsk-tipping.no/api-tipping/getResultInfo.json?gameDay=010"},
+                { "fotballtippingMidt", "https://www.norsk-tipping.no/api-tipping/getResultInfo.json?gameDay=001"},
+                { "superlotto", "https://www.norsk-tipping.no/api-lotto/getSuperLottoResultInfo.json"}
+        };
+
+        public ResultatData(string spillnavn)
+        {
+            
+            this.Spillnavn = spillnavn;
+        }
+        public string Spillnavn { get; protected set; }
+    }
+}
---
+++ Tipperesultater/DataModel/SuperLottoData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Windows.Data.Json;
+
+namespace Tipperesultater.Data
+{
+    public class SuperLottoData : ResultatData
+    {
+
+       public SuperLottoData(JsonObject jsonObjectLotto, string spillnavn, LottoData lottoData) : base(spillnavn)
+      {
+          JsonArray arr = jsonObjectLotto["superlottoDraws"].GetArray();
+          JsonObject sisteTrekning = arr[0].GetObject(); 
+
+           var a = sisteTrekning["drawDate"].GetString();
+           DateTime trekningspunkt = DateTime.ParseExact(a, "yyyy,MM,dd,HH,mm,ss", CultureInfo.CurrentCulture);
+           string trekningspunktAsString = trekningspunkt.ToString("dddd d. MMMM", CultureInfo.CurrentCulture);
+
+           
+           JsonObject jObject = sisteTrekning["superlottoResult"].GetObject();
+           this.AntallVinnere = jObject["numberOfWinners"].GetNumber().ToString();
+           double prizeAsDouble = jObject["prizeAmount"].GetNumber();
+           String prizeAsString = prizeAsDouble.ToString("### ### ### kr");
+           this.Premie = prizeAsString;
+           this.Trekningsdato = trekningspunktAsString;
+           this.Vinnere = String.Join("\r\n", jObject["winnerList"].GetArray().Select(x =>
+                           int.Parse(x.GetArray()[0].GetString()).ToString("### ### ###") + ": " + decodeGender(x.GetArray()[1].GetString()) + ", " + UpperFirst(x.GetArray()[4].GetString()) + " " + x.GetArray()[6].GetString()
+                        ).ToList());
+           this.NesteTrekning = String.Format("{0} ({1} trekninger til)", lottoData.NesteSuperLottoTrekning, lottoData.AntallTrekningerTilNesteSuperLotto);
+           
+       }
+
+       private string UpperFirst(string text)
+       {
+           return char.ToUpper(text[0]) +
+               ((text.Length > 1) ? text.Substring(1).ToLower() : string.Empty);
+       }
+
+       private string decodeGender(string p)
+       {
+           if (p.Equals("K"))
+           {
+               return "Kvinne";
+           }
+           return "Mann";
+       }
+
+
+       public string AntallVinnere { get; protected set; }
+       public string Trekningsdato { get; protected set; }
+       public string Premie { get; protected set; }
+       public string Vinnere { get; protected set; }
+       public string NesteTrekning { get; protected set; }
+      
+    }
+
+}
+
+
